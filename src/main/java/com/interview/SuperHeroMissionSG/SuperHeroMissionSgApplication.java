ec5e7890e002ec39b08040906dc0bf20eccec9b2
Andres <andres.felipe.rincon.gamboa@sap.com>
2019-06-15 14:33:13 -0400

    first commit

---
+++ src/main/java/com/interview/SuperHeroMissionSG/SuperHeroMissionSgApplication.java
@@ -0,0 +1,13 @@
+package com.interview.SuperHeroMissionSG;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperHeroMissionSgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperHeroMissionSgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/interview/SuperHeroMissionSG/SuperHeroMissionSgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.interview.SuperHeroMissionSG;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperHeroMissionSgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
