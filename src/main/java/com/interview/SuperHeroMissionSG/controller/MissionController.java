e89f4f8c547b9cc727265b3f6d78b3511905fcc1
Andres <andres.felipe.rincon.gamboa@sap.com>
2019-06-16 10:39:12 -0400

    adding getAllMissionsInfo

--- src/main/java/com/interview/SuperHeroMissionSG/controller/MissionController.java
+++ src/main/java/com/interview/SuperHeroMissionSG/controller/MissionController.java
@@ -3,6 +3,9 @@ package com.interview.SuperHeroMissionSG.controller;
 import com.interview.SuperHeroMissionSG.model.Mission;
 import com.interview.SuperHeroMissionSG.repository.MissionRepository;
 import java.util.Map;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,13 +21,28 @@ public class MissionController {
     MissionRepository missionRepository;
     
     @RequestMapping(value="/createMission", method = RequestMethod.POST)
-    public ResponseEntity<String> createMission(@RequestParam Map<String, String> parameters){
-        missionRepository.save(Mission.builder().isCompleted(parameters.get("isCompleted").equals("true"))
-                                                .isDeleted(parameters.get("isDeleted").equals("true"))
-                                                .missionName(parameters.get("missionName"))
-                                                .superHeroName(parameters.get("superHeroName"))
+    public ResponseEntity<String> createMission(@RequestParam Map<String, Object> parameters){
+        missionRepository.save(Mission.builder().isCompleted(Boolean.getBoolean((String) parameters.get("isCompleted")))
+                                                .isDeleted(Boolean.getBoolean((String) parameters.get("isDeleted")))
+                                                .missionName((String) parameters.get("missionName"))
+                                                .superHeroName((String) parameters.get("superHeroName"))
                                                 .build()
                                                );
         return new ResponseEntity<>("", HttpStatus.OK);
+    }
+    @RequestMapping(value="/getAllMissions", method= RequestMethod.GET)
+    public ResponseEntity<String> getAllMissions() throws JSONException{
+        
+        JSONArray missions= new JSONArray();
+        for(Mission mission: missionRepository.findAll()){
+            JSONObject missionJson = new JSONObject();
+            missionJson.put("missionName", mission.getSuperHeroName());
+            missionJson.put("superHeroName", mission.getSuperHeroName());
+            missionJson.put("isCompleted", mission.isCompleted());
+            missionJson.put("isDeleted", mission.isDeleted());
+            missions.put(missionJson);
+        }
+        return new ResponseEntity<>(missions.toString(), HttpStatus.OK);
+        
     }
 }
